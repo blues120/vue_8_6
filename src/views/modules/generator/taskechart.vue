bebf0f0627371d02001b085b25d55d9b2110c2b0
zhangwei <956898678@qq.com>
2019-08-22 08:32:34 +0800

    编译成功

--- src/views/modules/generator/taskechart.vue
+++ src/views/modules/generator/taskechart.vue
@@ -22,61 +22,41 @@
     data () {
       return {
         chartLine: null,
-        taskId: 0
+        taskId: 0,
+        option: {}
+
       }
     },
     mounted () {
-      this.initChart()
-    },
-    activated () {
+      debugger
       this.taskId = this.$route.query.taskId
       if (this.taskId === undefined) {
         this.taskId = 0
       }
+      this.initChart()
+    },
+    activated () {
       if (this.chartLine) {
         this.chartLine.resize()
       }
     },
     methods: {
       initChart () {
-        var option = {
-          tooltip: {         // Option config. Can be overwrited by series or data
-            trigger: 'axis',
-            // show: true,   //default true
-            showDelay: 0,
-            hideDelay: 50,
-            transitionDuration: 0,
-            backgroundColor: 'rgba(255,0,255,0.7)',
-            borderColor: '#f50',
-            borderRadius: 8,
-            borderWidth: 2,
-            padding: 10,    // [5, 10, 15, 20]
-            position: function (p) {
-              // 位置回调
-              // console.log && console.log(p);
-              return [p[0] + 10, p[1] - 10]
-            },
-            textStyle: {
-              color: 'yellow',
-              decoration: 'none',
-              fontFamily: 'Verdana, sans-serif',
-              fontSize: 15,
-              fontStyle: 'italic',
-              fontWeight: 'bold'
-            },
-            formatter: function (params, ticket, callback) {
-              console.log(params)
-              var res = ' ' + params[0].name
-              for (var i = 0, l = params.length; i < l; i++) {
-                res += '<br/>' + params[i].seriesName + ' : ' + params[i].value
-              }
-              setTimeout(function () {
-                // 仅为了模拟异步回调
-                callback(ticket, res)
-              }, 500)
-              return 'loading'
-            }
-            // formatter: "Template formatter: <br/>{b}<br/>{a}:{c}<br/>{a1}:{c1}"
+        this.option = {
+          title: {
+            'text': '',
+            'subtext': ''
+          },
+          tooltip: {
+            trigger: 'item'
+          },
+          dataZoom: {
+            show: true,
+            realtime: true,
+            orient: 'vertical',   // 'horizontal'
+            x: 0,
+            start: 40,
+            end: 60
           },
           toolbox: {
             show: true,
@@ -100,75 +80,42 @@
             }
 
           },
-          'legend': {
-            'data': ['坐标轴触发1', '坐标轴触发2', '数据项触发1', '数据项触发2']
+          legend: {
+            data: []
           },
           calculable: true,
           xAxis: {
-            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+            data: []
           },
           yAxis: {
             type: 'value'
           },
           series: [
-            {
-              name: '坐标轴触发1',
-              type: 'bar',
-              data: [
-                {value: 320, extra: 'Hello~'},
-                332, 301, 334, 390, 330, 320
-              ]
-            },
-            {
-              name: '坐标轴触发2',
-              type: 'bar',
-              data: [862, 1018, 964, 1026, 1679, 1600, 157]
-            },
-            {
-              name: '数据项触发1',
-              type: 'bar',
-              tooltip: {             // Series config.
-                trigger: 'item',
-                backgroundColor: 'black',
-                position: [0, 0],
-                formatter: 'Series formatter: <br/>{a}<br/>{b}:{c}'
-              },
-              stack: '数据项',
-              data: [
-                120, 132,
-                {
-                  value: 301,
-                  itemStyle: {normal: {color: 'red'}},
-                  tooltip: {     // Data config.
-                    backgroundColor: 'blue',
-                    formatter: 'Data formatter: <br/>{a}<br/>{b}:{c}'
-                  }
-                },
-                134, 90,
-                {
-                  value: 230,
-                  tooltip: {show: false}
-                },
-                210
-              ]
-            },
-            {
-              name: '数据项触发2',
-              type: 'bar',
-              tooltip: {
-                show: false,
-                trigger: 'item'
-              },
-              stack: '数据项',
-              data: [150, 232, 201, 154, 190, 330, 410]
-            }
+
           ]
         }
 
         this.chartLine = echarts.init(document.getElementById('J_chartLineBox'))
-        this.chartLine.setOption(option)
+        this.chartLine.setOption(this.option)
         window.addEventListener('resize', () => {
           this.chartLine.resize()
+        })
+
+        this.$http({
+          url: this.$http.adornUrl(`/generator/ttask/getTaskEchartData`),
+          method: 'get',
+          params: this.$http.adornParams({
+            taskId: this.taskId
+          })
+        }).then(({data}) => {
+          if (data && data.code === 0) {
+            this.option.legend.data = data.legend
+            this.option.xAxis.data = data.xAxis
+            this.option.series.push(data.series)
+            this.option.title.text = data.group.name
+            debugger
+            this.chartLine.setOption(this.option)
+          }
         })
       }
     }
